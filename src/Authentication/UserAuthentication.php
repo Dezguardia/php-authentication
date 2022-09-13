0990450d463e84ce38c8b21dae15e5a8b5f443e4
Gabriel Houdry--Bohême <gabriel.houdry--boheme@etudiant.univ-reims.fr>
2022-09-13 09:59:18 +0200

    Page auth.php et formulaire opérationnels

--- src/Authentication/UserAuthentication.php
+++ src/Authentication/UserAuthentication.php
@@ -2,10 +2,48 @@
 
 namespace Authentication;
 
+use Authentication\Exception\AuthenticationException;
+use Entity\Exception\EntityNotFoundException;
+use Entity\User;
+
 class UserAuthentication
 {
     private const LOGIN_INPUT_NAME = "login";
     private const PASSWORD_INPUT_NAME = "password";
 
 
+    public function loginForm(string $action, string $submitText='OK'): string
+    {
+        $logInput=$this::LOGIN_INPUT_NAME;
+        $passInput=$this::PASSWORD_INPUT_NAME;
+        $form = <<<HTML
+        <form name="loginform" method="POST" action="{$action}">
+            <label>
+                <input name="login" type="text" placeholder="$logInput">
+            </label>
+            <label>
+                <input name="pass" type="password" placeholder="$passInput">
+            </label>
+        
+            <button type="submit">{$submitText}</button>
+        </form>
+        HTML;
+        return $form;
+    }
+
+    /**
+     * @throws AuthenticationException
+     * @throws EntityNotFoundException
+     */
+    public function getUserFromAuth(): User
+    {
+        $login=$_POST['login'];
+        $pass=$_POST['pass'];
+        try {
+            $user = User::findByCredentials($login, $pass);
+        } catch (EntityNotFoundException) {
+            throw new AuthenticationException("Utilisateur introuvable.");
+        }
+        return $user;
+    }
 }
