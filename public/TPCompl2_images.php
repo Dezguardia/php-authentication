7f6bdde0b148524ed3ffcd827902c35d7c2fdca7
Gabriel Houdry--Bohême <gabriel.houdry--boheme@etudiant.univ-reims.fr>
2022-09-29 11:16:00 +0200

    Fin de séance - lune fonctionnelle tp complémentaire

--- public/TPCompl2_images.php
+++ public/TPCompl2_images.php
@@ -3,6 +3,8 @@
 use Html\WebPage;
 
 $p = new WebPage('Images');
+$p->appendCssUrl("css/style.css");
+$moonValue = $_POST['moonValue'] ?? 80;
 $p->appendContent(
     <<<HTML
     <h2>Cible</h2>
@@ -10,8 +12,15 @@ $p->appendContent(
     <h2>Cercles</h2>
     <img src="imageScripts/circles.php" alt="circles image">
     <h2>Ciel</h2>
-    <img src="imageScripts/sky.php" alt="sky image">
+    <form name="moonValue" action="TPCompl2_images.php" method="POST">
+    <label>
+    Phase de la lune :
+        <input name="moonValue" type="range" min="0" max="200" step="20" value="$moonValue" required>
+    </label>
+    <button type="submit">Changer</button>
+    </form>
+    <img src="imageScripts/sky.php?moonValue=$moonValue" alt="sky image">
+    <h2>Filtres</h2>
 HTML
-
 );
 echo $p->toHTML();
--- public/imageScripts/sky.php
+++ public/imageScripts/sky.php
@@ -8,6 +8,14 @@ $img= MyGdImage::createFromSize(500, 500);
 $background= $img->colorallocate(0, 0, 0);
 $img->filledrectangle(250, 250, 500, 500, $background);
 $yellow = $img->colorallocate(250, 246, 134);
+$blackValue=0;
+$yVal=0;
+for ($i=0;$i<40;$i++) {
+    $color = $img->colorallocate($blackValue, $blackValue, $blackValue);
+    $img->filledrectangle(0, $yVal, 500, $yVal+500/40, $color);
+    $blackValue+=2;
+    $yVal+=500/40;
+}
 for ($i=0;$i<30;$i++) {
     $starsize=rand(1, 4);
     $img->filledellipse(rand(0, 250), rand(0, 250), $starsize, $starsize, $yellow);
@@ -15,10 +23,22 @@ for ($i=0;$i<30;$i++) {
     $img->filledellipse(rand(0, 250), rand(250, 500), $starsize, $starsize, $yellow);
     $img->filledellipse(rand(250, 500), rand(0, 250), $starsize, $starsize, $yellow);
 }
+$moonValue = $_GET['moonValue'] ?? 80;
+$width=130-(130/100)*(200-$moonValue);
 
-$img->filledellipse(150, 250, 125, 125, $yellow);
-$img->filledarc(150, 250, 25, 125, 90, 90, $background, IMG_ARC_PIE);
-$img->filledellipse(350, 250, 125, 125, $yellow);
-$img->filledarc(350, 250, 25, 125, 90, 90, $background, IMG_ARC_PIE);
+$arcColor = $background;
+$moonColor = $yellow;
+$angle=-90;
+$end_angle=90;
+if ($moonValue>100) {
+    $arcColor=$yellow;
+    $moonColor = $background;
+    $angle=90;
+    $end_angle=-90;
+}
+$img->filledellipse(150, 250, 125, 125, $moonColor);
+$img->filledarc(150, 250, 125, 125, $angle, $end_angle, $arcColor, IMG_ARC_PIE);
+$img->filledarc(150, 250, $width, 126, 120, 120, $arcColor, IMG_ARC_PIE);
+//$img->filter(IMG_FILTER_NEGATE, IMG_FILTER_BRIGHTNESS);
 header("Content-type: image/png");
 $img->png();
